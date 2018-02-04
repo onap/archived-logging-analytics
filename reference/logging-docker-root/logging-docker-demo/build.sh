d65f948640ebf71bb9fb93ea45631d4573aead5d
Michael O'Brien <frank.obrien@amdocs.com>
2018-02-04 11:01:41 -0500

    add mock service docker/filebeat
    Change-Id: Id2fa50e20a0a1ac2fc048d954bcd32ca5c4adaf3
    Signed-off-by: Michael O'Brien <frank.obrien@amdocs.com>
    Issue-ID: LOG-137

--- reference/logging-docker-root/logging-docker-demo/build.sh
+++ reference/logging-docker-root/logging-docker-demo/build.sh
@@ -1,8 +1,9 @@
 mkdir target
+# get war for docker insertion
 cp ../../logging-demo/target/*.war target
 docker build -t oomk8s/logging-demo-nbi -f DockerFile .
 docker images | grep logging-demo-nbi
-docker tag oomk8s/logging-demo-nbi oomk8s/logging-demo-nbi:0.0.1
+docker tag oomk8s/logging-demo-nbi oomk8s/logging-demo-nbi:0.0.2
 docker login
-docker push oomk8s/logging-demo-nbi:0.0.1
+docker push oomk8s/logging-demo-nbi:0.0.2
 #docker run -d -it --rm -p 8888:8080 oomk8s/logging-demo-nbi:latest
