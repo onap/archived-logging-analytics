8db3c85d8d99e69a67159eb24cb03fc583929b9f
Michael O'Brien <frank.obrien@amdocs.com>
2018-04-26 11:18:51 -0400

    expanding log lib code coverage
    Change-Id: I99f2852c7d1128f439ccfaaa6fad4618ec16d1d7
    Signed-off-by: Michael O'Brien <frank.obrien@amdocs.com>
    Issue-ID: LOG-137

---
+++ reference/logging-demo/src/test/java/org/onap/demo/logging/LoggingTestRunner.java
@@ -0,0 +1,35 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.logging
+ * ================================================================================
+ * Copyright © 2018 Amdocs
+ * All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.demo.logging;
+
+import org.junit.Test;
+import org.springframework.util.Assert;
+
+public class LoggingTestRunner {
+    @Test
+    public void testHealth() {
+        RestServiceImpl impl = new RestServiceImpl();
+        System.out.println("testing RestServiceImpl");
+        Assert.notNull(impl);
+        impl.getTest();
+    }
+}
