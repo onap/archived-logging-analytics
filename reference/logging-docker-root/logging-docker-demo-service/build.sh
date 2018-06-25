9eee6ac9daa5b82946b4f7c7eaa817ae3e1f4cab
Michael O'Brien <frank.obrien@amdocs.com>
2018-06-25 13:59:31 -0400

    logging library use and k8s
    Change-Id: I264f6dd5270543a216612c50fd5806458e0e806a
    Issue-ID: LOG-135
    Signed-off-by: Michael O'Brien <frank.obrien@amdocs.com>

--- reference/logging-docker-root/logging-docker-demo-service/build.sh
+++ reference/logging-docker-root/logging-docker-demo-service/build.sh
@@ -20,12 +20,12 @@
 # source from https://jira.onap.org/browse/LOG-135
 # Michael O'Brien
 
-
 mkdir target
+# reuse the same war for the service pods
 cp ../../logging-demo/target/*.war target
-docker build -t oomk8s/logging-demo-nbi -f DockerFile .
-docker images | grep logging-demo-nbi
-docker tag oomk8s/logging-demo-nbi oomk8s/logging-demo-nbi:0.0.3
+docker build -t oomk8s/logging-demo-service -f DockerFile .
+docker images | grep logging-demo-service
+docker tag oomk8s/logging-demo-service oomk8s/logging-demo-service:0.0.3
 docker login
-docker push oomk8s/logging-demo-nbi:0.0.3
-#docker run -d -it --rm -p 8888:8080 oomk8s/logging-demo-nbi:latest
+docker push oomk8s/logging-demo-service:0.0.3
+#docker run -d -it --rm -p 8888:8080 oomk8s/logging-demo-service:latest
