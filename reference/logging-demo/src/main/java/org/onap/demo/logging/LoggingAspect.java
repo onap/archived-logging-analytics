837ffeaa8bd1e2d96f47ed55a32ba35216311321
Michael O'Brien <frank.obrien@amdocs.com>
2018-06-25 13:59:31 -0400

    logging library use and k8s
    Change-Id: I264f6dd5270543a216612c50fd5806458e0e806a
    Issue-ID: LOG-135
    Signed-off-by: Michael O'Brien <frank.obrien@amdocs.com>

---
+++ reference/logging-demo/src/main/java/org/onap/demo/logging/LoggingAspect.java
@@ -0,0 +1,55 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.logging
+ * ================================================================================
+ * Copyright © 2018 Amdocs
+ * All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.demo.logging;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.onap.logging.ref.slf4j.ONAPLogAdapter;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+public class LoggingAspect {
+    
+    @Before("execution(* org.onap.demo.logging.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        Object servletRequest = args[0];
+        ONAPLogAdapter.HttpServletRequestAdapter requestAdapter = 
+                new ONAPLogAdapter.HttpServletRequestAdapter((HttpServletRequest)servletRequest);
+        final ONAPLogAdapter adapter = new ONAPLogAdapter(
+                LoggerFactory.getLogger(joinPoint.getTarget().getClass()));
+        try {
+            adapter.entering(requestAdapter);
+        } finally {
+        }
+    }
+    
+    @After("execution(* org.onap.demo.logging.*.*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+        final ONAPLogAdapter adapter = new ONAPLogAdapter(
+                LoggerFactory.getLogger(joinPoint.getTarget().getClass()));
+        adapter.exiting();
+    }
+}
