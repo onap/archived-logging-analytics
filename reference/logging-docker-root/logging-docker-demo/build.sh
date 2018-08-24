4671f5f5afc8ade26df727651373597f25bef277
Michael O'Brien <frank.obrien@amdocs.com>
2018-08-24 15:03:20 -0400

    standard logback.xml pattern
    Change-Id: I06bc3f660fc4c28a53e8f9353d638b87e6220611
    Issue-ID: LOG-630
    Signed-off-by: Michael O'Brien <frank.obrien@amdocs.com>

--- reference/logging-docker-root/logging-docker-demo/build.sh
+++ reference/logging-docker-root/logging-docker-demo/build.sh
@@ -25,7 +25,7 @@ mkdir target
 cp ../../logging-demo/target/*.war target
 docker build -t oomk8s/logging-demo-nbi -f DockerFile .
 docker images | grep logging-demo-nbi
-docker tag oomk8s/logging-demo-nbi oomk8s/logging-demo-nbi:0.0.3
+docker tag oomk8s/logging-demo-nbi oomk8s/logging-demo-nbi:0.0.4
 docker login
-docker push oomk8s/logging-demo-nbi:0.0.3
+docker push oomk8s/logging-demo-nbi:0.0.4
 #docker run -d -it --rm -p 8888:8080 oomk8s/logging-demo-nbi:latest
