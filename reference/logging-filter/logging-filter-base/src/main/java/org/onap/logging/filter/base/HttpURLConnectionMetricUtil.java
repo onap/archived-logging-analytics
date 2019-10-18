a7b956be29b54cc626621ea1388c7fa8bdc45efb
Smokowski, Kevin (ks6305) <kevin.smokowski@att.com>
2019-10-18 18:35:10 +0000

    HttpURLConnection metric support
    add java.net.HttpURLConnection metric util
    
    Issue-ID: LOG-1172
    Signed-off-by: Smokowski, Kevin (ks6305) <kevin.smokowski@att.com>
    Change-Id: I60b1c59d1dd40f44a92dc1e92e406129830abf91

---
+++ reference/logging-filter/logging-filter-base/src/main/java/org/onap/logging/filter/base/HttpURLConnectionMetricUtil.java
@@ -0,0 +1,82 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - Logging
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.logging.filter.base;
+
+import java.net.HttpURLConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpURLConnectionMetricUtil
+        extends AbstractMetricLogFilter<HttpURLConnection, HttpURLConnection, HttpURLConnection> {
+    protected static final Logger logger = LoggerFactory.getLogger(HttpURLConnectionMetricUtil.class);
+
+    public void logBefore(HttpURLConnection request, ONAPComponentsList targetEntity) {
+        setTargetEntity(targetEntity);
+        pre(request, request);
+    }
+
+    public void logAfter(HttpURLConnection request) {
+        post(request, request);
+    }
+
+    @Override
+    protected String getTargetServiceName(HttpURLConnection request) {
+        return request.getURL().getPath();
+    }
+
+    @Override
+    protected int getHttpStatusCode(HttpURLConnection response) {
+        try {
+            return response.getResponseCode();
+        } catch (Exception e) {
+            logger.error("getHttpStatusCode failed, defaulting to 500", e);
+        }
+        return 500;
+    }
+
+    @Override
+    protected String getResponseCode(HttpURLConnection response) {
+        try {
+            return String.valueOf(response.getResponseCode());
+        } catch (Exception e) {
+            logger.error("getResponseCode failed, defaulting to 500", e);
+        }
+        return "500";
+    }
+
+    @Override
+    protected String getTargetEntity(HttpURLConnection request) {
+        return Constants.DefaultValues.UNKNOWN_TARGET_ENTITY;
+    }
+
+    public void pre(HttpURLConnection request) {
+        pre(request, null);
+    }
+
+    public void filter(HttpURLConnection request, HttpURLConnection response) {
+        post(request, response);
+    }
+
+    @Override
+    protected void addHeader(HttpURLConnection request, String headerName, String headerValue) {
+        request.setRequestProperty(headerName, headerValue);
+    }
+}
